1304c7c81dcc434b85debde79f56ce07add9d0a1
Fenet_Ab <abilufenet@gmail.com>
2026-04-24 04:20:10 +0300

    user model dummy added

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,12 +3,14 @@ enum UserRole { admin, cashier, manager }
 class User {
   final String id;
   final String name;
+  final String pin;
   final String email;
   final UserRole role;
 
   User({
     required this.id,
     required this.name,
+    required this.pin,
     required this.email,
     required this.role,
   });
@@ -17,12 +19,30 @@ class User {
     return User(
       id: json['id'],
       name: json['name'],
+      pin: json['pin'],
       email: json['email'],
       role: json['role'],
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'name': name, 'role': role};
+    return {'id': id, 'name': name, 'pin': pin, 'email': email, 'role': role};
   }
+
+  static final List<User> dummyUsers = [
+    User(
+      id: '1',
+      name: 'Admin User',
+      pin: '123456',
+      email: 'admin@savvy.com',
+      role: UserRole.admin,
+    ),
+    User(
+      id: '2',
+      name: 'Cashier User',
+      pin: '654321',
+      email: 'cashier@savvy.com',
+      role: UserRole.cashier,
+    ),
+  ];
 }
